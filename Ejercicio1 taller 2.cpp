fa6afe6b89accd1795bafcf60acd9c3ecda4aa7b
Daniel Moreno <daniel970508@gmail.com>
2017-08-17 18:27:12 -0500

    Update Ejercicio1 taller 2.cpp

--- Ejercicio1 taller 2.cpp
+++ Ejercicio1 taller 2.cpp
@@ -3,21 +3,31 @@
 #include <stdio.h>
 
 using namespace std;
-int main (int argc, char** argv){
-	int n,i,j,tam;
-	int *a=0;
-	cin>> n;
-	for(i=0;i<n;i++){
-		a=new int[n];
-		for(j=0;j<n;j++){
-			a[j]=j*i;
-			cout<<a[j];
-		}
-		//tam=sizeof(a);
-		delete a;	
-		
-	}
-		tam= tam+ sizeof(n)+sizeof(i)*n+sizeof(j)*n;
-		cout<<" "<<tam;
+
+	struct nodo{
+		int x,y;
+		nodo *sig;
+	};
 	
+int main (int argc, char** argv){
+	int n, i;
+
+	nodo *cab = NULL, *aux;
+	cin>>n;
+	for(i=0; i<n; i++){
+		aux = new nodo;
+		aux -> x = i;
+		aux -> y = n - i;
+		aux -> sig = cab;
+		cab = aux;
+	}
+	aux = cab;
+	while (aux!=NULL){
+		cout<< aux->x<< aux->y<<endl;
+		aux = aux -> sig;
+	}
+	for(aux=cab->sig; aux!=NULL; aux=aux->sig){
+		delete cab;
+		cab = aux;
+	}
 }
