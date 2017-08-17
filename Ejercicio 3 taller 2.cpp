c9719c1b7137feb35cbe692d14e4b1912ab21071
Daniel Moreno <daniel970508@gmail.com>
2017-08-17 18:36:12 -0500

    Update Ejercicio 3 taller 2.cpp

--- Ejercicio 3 taller 2.cpp
+++ Ejercicio 3 taller 2.cpp
@@ -3,14 +3,31 @@
 #include <stdio.h>
 
 using namespace std;
-int main (int argc, char** argv){
-	int n;
+
 	struct nodo{
-		int x,4;
+		int x,y;
 		nodo *sig;
+	};
+	
+int main (int argc, char** argv){
+	int n, i;
+
+	nodo *cab = NULL, *aux;
+	cin>>n;
+	for(i=0; i<n; i++){
+		aux = new nodo;
+		aux -> x = i;
+		aux -> y = n - i;
+		aux -> sig = cab;
+		cab = aux;
 	}
-	nodo* cab= null, * aux;
-	cin>>n
-	for(i=o;i<n;i++){
-		aux
+	aux = cab;
+	while (aux!=NULL){
+		cout<< aux->x<< aux->y<<endl;
+		aux = aux -> sig;
 	}
+	for(aux=cab->sig; aux!=NULL; aux=aux->sig){
+		delete cab;
+		cab = aux;
+	}
+}
