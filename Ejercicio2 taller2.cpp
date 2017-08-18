fc7cc68778a11b58c5a8029082cb34a37eb7faa9
Juan Esteban Botello <juaneskill98@gmail.com>
2017-08-17 20:46:25 -0500

    Update Ejercicio2 taller2.cpp

--- Ejercicio2 taller2.cpp
+++ Ejercicio2 taller2.cpp
@@ -6,20 +6,24 @@ using namespace std;
 int main (int argc, char** argv){
 	int n,i,j;
 	int **a=0;
-	cin>>n;
+	
+	for(int cont=5;cont<=50;cont+=5){
+	n=cont;
 	a= new int *[n];
-	for(i=0;i<n;i++){
-		a[i]=new int[n];
-		for(j=0;j<n;j++){
-			a[i][j]=i*j;	
-		}
-	}
 		for(i=0;i<n;i++){
+			a[i]=new int[n];
 			for(j=0;j<n;j++){
-				cout<< a[i][j];
+				a[i][j]=i*j;	
 			}
-			delete a[i];
 		}
-	int tam=sizeof(n)+si
+			for(i=0;i<n;i++){
+				for(j=0;j<n;j++){
+					cout<< a[i][j];
+				}
+				delete a[i];
+			}
+		int tam=sizeof(n)+sizeof(i)*n*n+sizeof(j)+sizeof(j)*n+sizeof(a);
+		cout<<" "<<tam<<endl;
+}
 }
 
