c9bf673b6e65c6eec070e76f98956f7ec832aa10
Juan Esteban Botello <juaneskill98@gmail.com>
2017-09-14 22:11:29 -0500

    Update algoritmo3.cpp

--- algoritmo3.cpp
+++ algoritmo3.cpp
@@ -1,11 +1,29 @@
-#include <iostream>
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
 #include <stdio.h>
 
 using namespace std;
+double PCFreq = 0.0;
+__int64 CounterStart = 0;
 
+void StartCounter()
+{
+    LARGE_INTEGER li;
+    if(!QueryPerformanceFrequency(&li))
+    cout << "QueryPerformanceFrequency failed!\n";
+
+    PCFreq = double(li.QuadPart);
+
+    QueryPerformanceCounter(&li);
+    CounterStart = li.QuadPart;
+}
+double GetCounter()
+{
+    LARGE_INTEGER li;
+    QueryPerformanceCounter(&li);
+    return double(li.QuadPart-CounterStart)/PCFreq;
+}
 calcular(int a, int b){
 	if (b == 0){
 		return a;
@@ -16,8 +34,14 @@ calcular(int a, int b){
 }
 
 int main(){
-	int a, b;
+	int a, b,z;
 	cin>>a;
 	cin>>b;
-	cout<<endl<<calcular(a, b);
+	StartCounter();
+	z= calcular(a,b);
+	
+	cout << GetCounter() <<"\n";
+	
+	cout<<z<<endl;
+	
 }
