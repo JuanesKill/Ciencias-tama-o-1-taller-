6feeca671f0ae572ab26282a8e61296ab92240a5
Juan Esteban Botello <juaneskill98@gmail.com>
2017-09-14 22:11:57 -0500

    Update algoritmo1.cpp

--- algoritmo1.cpp
+++ algoritmo1.cpp
@@ -6,17 +6,39 @@
 
 using namespace std;
 
+double PCFreq = 0.0;
+__int64 CounterStart = 0;
+
+void StartCounter()
+{
+    LARGE_INTEGER li;
+    if(!QueryPerformanceFrequency(&li))
+    cout << "QueryPerformanceFrequency failed!\n";
+
+    PCFreq = double(li.QuadPart);
+
+    QueryPerformanceCounter(&li);
+    CounterStart = li.QuadPart;
+}
+double GetCounter()
+{
+    LARGE_INTEGER li;
+    QueryPerformanceCounter(&li);
+    return double(li.QuadPart-CounterStart)/PCFreq;
+}
 void f(const char * const prts){
 	if (prts[0] == '\0'){
 		return;
 	}
 	else{
 		f(&prts[1]);
-		cout<<prts[0];
+		cout<<prts[0]<<" ";
 	}
 }
 
 int main(){
-	char *prts = "daniel";
+	char *prts = "cincocincocincocincocincocincocincocincocincocincocincocincocincocincocincocincocincocincocincocinco";
+	StartCounter();
 	f(prts);
+	cout << GetCounter() <<"\n";
 } 
