83e55fde101fa753ed892cad1553943a5b349c5f
Juan Esteban Botello <juaneskill98@gmail.com>
2017-09-22 11:29:13 -0500

    Update Sumamatrices.cpp

--- Sumamatrices.cpp
+++ Sumamatrices.cpp
@@ -7,79 +7,61 @@
 #include <fstream>
 
 using namespace std;
-
- template<class T>
- void sumarmatrices(T**matriza,T**matrizb,int n){
- 	T**matrizc;
- 	for(int i=0;i<n;i++){
- 		for(int j=0;j<n;j++){
- 			T**matrizc=(**matriza+j)+(**matrizb+j);
- 			cout<<**matrizc<<" ";
- 			
-		 }
-		cout<< endl;
-		*matriza+i;
-		*matrizb+i;
-		*matrizc+i;
-	 }
- 	
- }
- int main(){
- 	int n;
- 	cout<< "Digite el tamaño de las matrices ";
- 	cin>>n;
- 	
- 	for(int i=0;i<n;i++){
- 		for(int j=0;j<n;j++){
- 			
- }
-}
-}
-//_________________________________________________________________________________________
-
-#include <cstdlib>
-#include <iostream>
-#include <time.h>
-#include <fstream>
-#include <stdio.h>
-#include <windows.h>
-#include <fstream>
-
-using namespace std;
+int n;
 
 template<class T>
-void Sumar(T **a, T **b, int n){
-	T **matrizc;
+void Sumar(T *a, T *b, int n){
+	T matrizc[5][5];
+	T *Pc;
+ 	Pc=matrizc[0];
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
-			cout<<a[i][j] + b[i][j]<<"	";
+			matrizc[i][j]= (*(a+j)+i) + ((*(b+j)+i));
+ 			cout<<*(*(matrizc+i)+j)<<"	";
+ 			
 		}
 		cout<< endl;
-//		*matriza+i;
-//		*matrizb+i;
-//		*matrizc+i;
+	
 	}
 }
+
+void LlenadoMatriz(int A[][5], int B[][5], int n){
+	int i, j;
+	srand(time(NULL));
+	for(i=0; i<n; i++){
+    	for(j=0; j<n; j++){
+			A[i][j]= rand() % 100;
+			B[i][j]= rand() % 100;
+        }
+    }
+    Sumar(*A, *B, n);
+}
+
 int main(){
-	int n;
+
 	cout<< "Digite el tamaño de las matrices nxn: ";
 	cin>>n;
 //	char a[n][n], b[n][n];
-	int **a[n][n], **b[n][n];
-//	srand(time(NULL));
+	int a[5][5], b[5][5], *Pa, *Pb;
+	Pa = a[0];
+	Pb = b[0];
+	LlenadoMatriz(a,b,5);
+	cout<< endl;	
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
-			//a[i][j] = 97 + rand() % (122 - 97);
-			cout<<"Digite el numero en la posicion ["<<i+1<<"] ["<<j+1<<"] de la primera matriz: ";
-			cin>>**a[i][j];
+			cout<< *(*(a+i)+j) << "	";
 		}
+    	cout<<endl;
 	}
+	cout<<endl;
 	for(int i=0; i<n; i++){
 		for(int j=0; j<n; j++){
-			//a[i][j] = 97 + rand() % (122 - 97);
-			cout<<"Digite el numero en la posicion ["<<i+1<<"] ["<<j+1<<"] de la primera matriz: ";
-			cin>>**b[i][j];
+			cout<< *(*(b+i)+j) << "	";
 		}
+    	cout<<endl;
 	}
-	Sumar(**a, **b, n);
+	
+
+//	Sumar(a, b, n);
+	
 }
