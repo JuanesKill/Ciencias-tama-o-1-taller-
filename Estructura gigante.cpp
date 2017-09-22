2ab427510bcca583e6ba69c901b0c016b8bb65ce
Juan Esteban Botello <juaneskill98@gmail.com>
2017-09-22 13:56:36 -0500

    Add files via upload

---
+++ Estructura gigante.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <stdio.h>
+#include <windows.h>
+#include <fstream>
+ 
+using namespace std;
+
+template<class T, class S>
+struct uno{
+	T x;
+	S y;
+};
+template<class W,class T ,class S>
+struct WU{
+	W **w;
+	uno<T,S> U;
+};
+
+template<class T1, class T,class S>
+struct P{
+	T1 t1;
+	uno<T,S> u1;
+};
+template<class T,class S,class F>
+struct L{
+	uno<T,S> n;
+	F *f;
+};
+template<class TL,class SL,class FL,class T1, class TH. class SH>
+struct O{
+	L<T,S,F> l;
+	P<T1,T,S> h;
+};
+template<class T,class S,class F>
+struct L{
+	uno<T,S> n;
+	F *f;
+};
+
+
+
+
+
+int main(){
+	S<int,int,char> s;
+	
+}
