e8d7499b3c676635d87519878599c7467bbb46be
Juan Esteban Botello <juaneskill98@gmail.com>
2017-10-02 13:44:40 -0500

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,17 +1,18 @@
 #ifndef LISTA_H
 #define LISTA_H
+template <class T>
 struct nodo{
 	T dato;
 	nodo<T> *sig;
-}
+};
 template <class T>
 class Lista{
 	nodo<T> *cabeza, *z;
-	
+	int tam;
 	public:
 		Lista(){
 			cabeza=z=NULL;
-			int tam;
+			tam=0;
 		}
 		void insertar_inicio(nodo<T> *info);
 		void insertar_final(nodo<T> *info);
@@ -22,13 +23,13 @@ class Lista{
 		T eliminar_pos(int pos);
 };
 	template <class T>
-	void Lista::insertar_inicio(nodo<T> *info){
+	void Lista<T>::insertar_inicio(nodo<T> *info){
 		info->sig=cabeza;
-		cabeza= info;
-				
+		cabeza=info;
+		tam++;				
 	}
 	template <class T>
-	void Lista::insertar_final(nodo<T> *info){
+	void Lista<T>::insertar_final(nodo<T> *info){
 		
 		nodo<T> *aux;
 		aux=cabeza;
@@ -44,10 +45,10 @@ class Lista{
 		tam++;
 	}
 	template <class T>
-	void Lista:: insertar_pos(nodo<T> *info, int pos){
+	void Lista<T>:: insertar_pos(nodo<T> *info, int pos){
 		nodo<T> *aux;
 		aux=cabeza;
-		if(cabeza<=1){
+		if(pos<=1){
 			insertar_inicio(info);
 		}else{
 			if(pos>tam){
@@ -60,28 +61,33 @@ class Lista{
 					}	
 				}	
 		}
+		tam++;
 	}
 	template <class T>
-	T Lista:: consultar(int pos){
-		if(cabeza!=NULL){
-			if(pos>tam || pos==0){
-				return 0;
-				}else{	for(int i=0;i<pos;i++){
-							aux= aux->sig;
-							info->sig=aux->sig;
-							aux->sig=info;	
-												
-					}
-				}
+	T Lista<T>:: consultar(int pos){
+		nodo<T> *aux;
+		aux=cabeza;
+		if(tam=1){
+			return aux->dato;
+		}else{
+		
+		if(pos>tam || pos==0){
+			return 0;
 			}else{
-				return 0;
-			}
+				int i=0;
+				while(aux!=NULL && i<pos){
+					aux=aux->sig;
+					i++;	
+				}
+			return aux->dato;
+		}
+	}
 	}	
-	template <class T>
-	T Lista:: eliminar_inicio(){
+/*	template <class T>
+	T Lista<T>:: eliminar_inicio(){
 		cabeza=	
 	
-	}
+	}*/
 		
 		
 		
