6cf2d9061b3a9457f8cd67a04165f2a7ccc5286c
Juan Esteban Botello <juaneskill98@gmail.com>
2017-10-02 11:48:51 -0500

    Update lista.h

--- lista.h
+++ lista.h
@@ -23,9 +23,9 @@ class Lista{
 };
 	template <class T>
 	void Lista::insertar_inicio(nodo<T> *info){
-		
 		info->sig=cabeza;
 		cabeza= info;
+				
 	}
 	template <class T>
 	void Lista::insertar_final(nodo<T> *info){
@@ -43,15 +43,50 @@ class Lista{
 		}
 		tam++;
 	}
+	template <class T>
 	void Lista:: insertar_pos(nodo<T> *info, int pos){
 		nodo<T> *aux;
-		if(cabeza!=NULL){
-			if(pos<=tam){
-				
-			}	
+		aux=cabeza;
+		if(cabeza<=1){
+			insertar_inicio(info);
+		}else{
+			if(pos>tam){
+				insertar_final(info);
+			} 
+			else{ for(int i=0;i<pos;i++){
+						aux= aux->sig;
+						info->sig=aux->sig;
+						aux->sig=info;				
+					}	
+				}	
 		}
 	}
+	template <class T>
+	T Lista:: consultar(int pos){
+		if(cabeza!=NULL){
+			if(pos>tam || pos==0){
+				return 0;
+				}else{	for(int i=0;i<pos;i++){
+							aux= aux->sig;
+							info->sig=aux->sig;
+							aux->sig=info;	
+												
+					}
+				}
+			}else{
+				return 0;
+			}
+	}	
+	template <class T>
+	T Lista:: eliminar_inicio(){
+		cabeza=	
 	
+	}
+		
+		
+		
+		
+		
 	
 	
 
