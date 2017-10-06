cea4531646c6a991e5e87062952c291edc393822
Juan Esteban Botello <juaneskill98@gmail.com>
2017-10-06 13:56:59 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,58 +3,78 @@
 #include <stdio.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
+struct Estud{
+	string nombre;
+	long int codigo;
+	string carrera;
+};
 
 int main(int argc, char** argv) {
-	////////LLENAR LISTA////////
-	Lista <int> lent;
+	/*Lista <int> lint;
+	Lista <float> lfloat;
 	nodo <int> *nuevo;
 	nuevo= new nodo <int>;
-	nuevo->dato=9;
-	lent.insertar_inicio(nuevo);
+	nuevo->dato=3;
 	nodo <int> *nuevo1;
-	nuevo1 = new nodo <int>;
+	nuevo1= new nodo <int>;
 	nuevo1->dato=8;
-	lent.insertar_inicio(nuevo1);
 	nodo <int> *nuevo2;
-	nuevo2 = new nodo <int>;
-	nuevo2->dato=20;
-	lent.insertar_final(nuevo2);
-	cout<<lent.consultar(1)<<" ";
-	cout<<lent.consultar(2)<<" ";
-	cout<<lent.consultar(3)<<" ";
-	cout<<lent.consultar(4)<<endl;
-	nodo<int> *nuevo3;
-	nuevo3 = new nodo <int>;
-	nuevo3->dato=12;
-	lent.insertar_pos(nuevo3,2);
-	////////CONSULTAR////////
-	cout<<lent.consultar(1)<<" ";
-	cout<<lent.consultar(2)<<" ";
-	cout<<lent.consultar(3)<<" ";
-	cout<<lent.consultar(4)<<endl;
-	
-//	lent.lista_vacia();
-//	cout<<lent.consultar(1)<<" ";
-//	cout<<lent.consultar(2)<<" ";
-//	cout<<lent.consultar(3)<<" ";
-//	cout<<lent.consultar(4)<<endl;
-//	lent.eliminar_final();
-//	cout<<lent.consultar(1)<<" ";
-//	cout<<lent.consultar(2)<<" ";
-//	cout<<lent.consultar(3)<<" ";
-//	cout<<lent.consultar(4)<<endl;	
-
-//	lent.eliminar_pos(2);
-//	cout<<lent.consultar(1)<<" ";
-//	cout<<lent.consultar(2)<<" ";
-//	cout<<lent.consultar(3)<<" ";
-//	cout<<lent.consultar(4)<<endl;
-	lent.eliminar_inicio();
-	cout<<lent.consultar(1)<<" ";
-	cout<<lent.consultar(2)<<" ";
-	cout<<lent.consultar(3)<<" ";
-	cout<<lent.consultar(4)<<endl;
-	
-	cout<<lent.tamano_lista()<<endl;
+	nuevo2= new nodo <int>;
+	nuevo2->dato=10;	
+	nodo <float> *nuevo3;
+	nuevo3= new nodo <float>;
+	nuevo3->dato=3.5;
+	nodo <float> *nuevo4;
+	nuevo4= new nodo <float>;
+	nuevo4->dato=8.2;
+	nodo <float> *nuevo5;
+	nuevo5= new nodo <float>;
+	nuevo5->dato=3.3;
+	lint.insertar_final(nuevo);
+	lint.insertar_inicio(nuevo1);
+	lint.insertar_pos(nuevo2,2);
+	lfloat.insertar_inicio(nuevo3);
+	lfloat.insertar_final(nuevo4);
+	lfloat.insertar_pos(nuevo5,2);
+	int i=1;
+	while(!lint.lista_vacia()){
+		cout<<lint.eliminar_pos(i);
+	}
+	system("pause");
+	cout<<lfloat.consultar(1);
+/*	while(!lfloat.lista_vacia()){
+		cout<< lfloat.eliminar_pos(i)<<" ";
+	}
+	cout<<lfloat.eliminar_pos(1);
+	*/
+	nodo <Estud> *a;
+	a= new nodo <Estud>;
+	nodo <Estud> *a1;
+	a1= new nodo <Estud>;
+	nodo <Estud> *a2;
+	a2= new nodo <Estud>;
+	Lista<Estud> lestud;
+	a->dato.nombre="pedro";
+	a->dato.codigo=20151020;
+	a->dato.carrera="sistemas";
+	lestud.insertar_pos(a,1);
+	a1->dato.nombre="juan";
+	a1->dato.codigo=2014204;
+	a1->dato.carrera="electrica";
+	lestud.insertar_inicio(a1);
+	a2->dato.nombre="daniel";
+	a2->dato.codigo=2012204;
+	a2->dato.carrera="industrial";
+	lestud.insertar_final(a2);
+	int i=1;
+	while(i<=lestud.tamano_lista()){
+		nodo <Estud> *resul;
+		resul= new nodo <Estud>;
+		resul->dato= lestud.consultar(i);
+		cout<< resul->dato.nombre;
+		i++;
+		delete resul;
+	}
+		
 	return 0;
 }
